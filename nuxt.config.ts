0b2378458b1cee27ba157bb2b9629de63f2da54e
HuseyinKocakusak <58011270+HuseyinKocakusak@users.noreply.github.com>
2024-08-26 21:37:14 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+export default {
+  target: 'static',  // This tells Nuxt to generate a static site
+  generate: {
+    dir: 'dist'  // This is the default output directory for static files
+  },
+  // Other configurations...
+}
