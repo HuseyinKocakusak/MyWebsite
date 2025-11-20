d1a53ec5490bfbb5451025a84587cfab0fe2bb37
HuseyinKocakusak <58011270+HuseyinKocakusak@users.noreply.github.com>
2025-11-21 00:35:56 +0300

    Delete nuxt.config.ts

--- nuxt.config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  target: 'static',  // This tells Nuxt to generate a static site
-  generate: {
-    dir: 'dist'  // This is the default output directory for static files
-  },
-  // Other configurations...
-}
