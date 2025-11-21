6f35510640ccc8bd2562a9966002f93d190e9499
HuseyinKocakusak <58011270+HuseyinKocakusak@users.noreply.github.com>
2025-11-22 01:35:58 +0300

    Prepare docs output for live site

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/MyWebsite/"   // must match my GitHub repo name
+  base: "/MyWebsite/", // must match my GitHub repo name
+  build: {
+    outDir: "docs",
+  },
 });
